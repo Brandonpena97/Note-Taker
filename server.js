f89e4bf0c87dd79c53768db13d52d0973c0deb62
Brandon Pena <brandonpena@gmail.com>
2022-01-27 20:17:37 -0500

    added app.post

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const { fstat } = require('fs');
 const path = require('path');
 const notes = require('./db/db.json')
 
 const PORT = 3001;
 
 const app = express(); // initialize an express server and call it 'app'
+
+function createNewNote (body, ) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname,'./db/db.json')
+    )
+
+}
 
 // middleware
 app.use(express.static('public'))
@@ -15,7 +25,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "./public/index.html"))
 })
 
-app.get('/notes',(req, res) => {
+app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, "./public/notes.html"))
 })
 
@@ -24,9 +34,14 @@ app.get('/api/notes', (req, res) => {
     res.json(notes)
 })
 
+app.post('./api/notes', (req, res) => {
 
+    notes.push(req, body);
 
+    fs.writeFileSync("./db/db.json", notes)
 
+    res.json(req.body)
+})
 
 
 app.listen(PORT, () => {
